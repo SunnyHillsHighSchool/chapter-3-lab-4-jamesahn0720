242ed75c539dd5cb2c0206e1fd3c17b8d4707a01
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-05-16 22:49:55 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 class Main {
   public static void main(String[] args) {
-      //add test cases  
+       PQTester test1 = new PQTester("one two three four five six seven"); 
+        PQTester test2 = new PQTester("1 2 3 4 5 one two three four five");
+        PQTester test3 = new PQTester("a p h j e f m c i d k l g n o b"); 
+
+        System.out.println("toString() - " + test1.toString()); 
+        System.out.println("getMin() - " + test1.getMin()); 
+        System.out.println("getNaturalOrder() - " + test1.getNaturalOrder()); 
+
+        System.out.println("toString() - " + test2.toString()); 
+        System.out.println("getMin() - " + test2.getMin()); 
+        System.out.println("getNaturalOrder() - " + test2.getNaturalOrder()); 
+
+        System.out.println("toString() - " + test3.toString()); 
+        System.out.println("getMin() - " + test3.getMin()); 
+        System.out.println("getNaturalOrder() - " + test3.getNaturalOrder());  
   }
 }
